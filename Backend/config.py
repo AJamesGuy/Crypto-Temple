aa135d7fad9d63e43be5076bab115e902311d472
Anthony Guy <anthonyyguyy@gmail.com>
2026-02-26 14:38:44 -0500

    Working on deployment

--- Backend/config.py
+++ Backend/config.py
@@ -15,5 +15,5 @@ class TestingConfig:
     TESTING = True
 
 class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite://app.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
     CACHE_TYPE = "SimpleCache"
