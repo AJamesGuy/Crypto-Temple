74709cc591a01fc851f4a29dc03dc4758c11228c
Anthony Guy <anthonyyguyy@gmail.com>
2026-02-26 23:08:27 -0500

    Another commit

--- flask_app.py
+++ flask_app.py
@@ -5,6 +5,6 @@ from app.scheduler import start_scheduler
 app = create_app('ProductionConfig')
 
 with app.app_context():
-    # db.drop_all()
+    # db.drop_all()  # Uncomment this line to drop all tables (use with caution)
     db.create_all()
     start_scheduler(app)  # Start the background scheduler for market data updates
